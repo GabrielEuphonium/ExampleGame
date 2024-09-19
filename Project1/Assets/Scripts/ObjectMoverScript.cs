73e0d9fc3bf5042089a5e5e8457950bba2891cc3
Gabriel Nelson <nelson3dagvmail@gmail.com>
2024-09-19 15:48:57 -0600

    Added transform script to cube

--- Project1/Assets/Scripts/ObjectMoverScript.cs
+++ Project1/Assets/Scripts/ObjectMoverScript.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectMoverScript : MonoBehaviour
+public class TransformController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        //ou24ugohoh24giyg13fougyigg4
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var x = Mathf.PingPong(Time.time, 3);
+        var p = new Vector3(0, x, 0);
+        transform.position = p;
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
     }
 }
