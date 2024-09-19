6d5518f4c452743a967eb653113d95a82e2a4b77
Gabriel Nelson <nelson3dagvmail@gmail.com>
2024-09-19 15:37:21 -0600

    checkig issues
    script changes take long time to load

--- Project1/Assets/Scripts/ObjectMoverScript.cs
+++ Project1/Assets/Scripts/ObjectMoverScript.cs
@@ -7,7 +7,7 @@ public class ObjectMoverScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ou24ugohoh24giyg13fougyigg4
     }
 
     // Update is called once per frame
