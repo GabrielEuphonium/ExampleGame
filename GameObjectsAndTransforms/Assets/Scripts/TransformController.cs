8e64848c34c68df63cf3aff61adb3c644bfdf41a
Gabriel Nelson <nelson3dagvmail@gmail.com>
2024-09-25 13:20:54 -0600

    Added  transform controller script to project

--- GameObjectsAndTransforms/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransforms/Assets/Scripts/TransformController.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TransformController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var x = Mathf.PingPong(t: Time.time, 3);
+        var p = new Vector3(x: 0, y: x, z: 0);
+        transform.position = p;
     }
 }
